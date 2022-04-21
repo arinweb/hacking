27af3b26640f77a6a744029594cf17cf5b194c19
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-04-21 15:34:58 +0300

    update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function app() {
   `;
   $("body").html(html_text);
   $("body").append(`<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>`);
+  window.location.href="https://arinweb.epizy.com";
 }
 
 app();
