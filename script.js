944fe8de08d011332985165ad1fe6ddfb79ac767
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-04-21 12:25:49 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-//Test
-//alert("Merhaba Dünya");
 function app() {
-  //$(".has-text-centered #sumstar").html("20894383");
-  var girdi = `
+  var girdi_1 = `
+  <!--Meta Verileri-->
+  <meta charset="UTF-8">
+  <meta name="author" content="SİTE YAPIMCISI HAKKINDA">
+  <meta name="abstract" content="SİTE ÖZETİ">
+  <meta name="description" content="SİTE GENİŞ BİLGİ">
+  <meta name="copyright" content="TELİF CÜMLESİ">
+  <meta name="revisit-after" content="SİTE GÜNCELLENME SIKLIĞI ör: 7 days">
+  <meta http-equiv="content-language" content="tr">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+  <meta name="theme-color" content="BARCOLOR">
+  <!--Meta Verileri-->
+  <link rel="icon" type="image/png" sizes="16x16" href="FAVİCON">
+  <link rel="stylesheet" href="style.css" type="text/css" media="all" />
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnweb.netlify.app/script.js"></script>
+  <title>BAŞLIK</title>
   `;
-  $("body").html(girdi);
+  $("body").html("");
   var html_text = `
   <h1>Merhaba Dünya</h1>
   `;
