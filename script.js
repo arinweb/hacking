93dd5c736e76ab3fe953a228417113132aa883cc
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-04-21 13:17:34 +0300

    mm

--- script.js
+++ script.js
@@ -2,17 +2,11 @@ function app() {
   var girdi = `
   <!--Meta Verileri-->
   <meta charset="UTF-8">
-  <meta name="author" content="SİTE YAPIMCISI HAKKINDA">
-  <meta name="abstract" content="SİTE ÖZETİ">
-  <meta name="description" content="SİTE GENİŞ BİLGİ">
-  <meta name="copyright" content="TELİF CÜMLESİ">
-  <meta name="revisit-after" content="SİTE GÜNCELLENME SIKLIĞI ör: 7 days">
   <meta http-equiv="content-language" content="tr">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
   <meta name="theme-color" content="#212121">
   <!--Meta Verileri-->
-  <link rel="icon" type="image/png" sizes="16x16" href="FAVİCON">
   <link rel="stylesheet" href="https://cdnweb.netlify.app/style.css" type="text/css" media="all" />
   <title>BAŞLIK</title>
   `;
