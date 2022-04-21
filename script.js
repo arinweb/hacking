5ea73543846bdb8f272f32caca0a6ab27df768d3
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-04-21 12:01:09 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Test
 //alert("Merhaba Dünya");
 function app() {
-  $(".has-text-centered #sumstar").html("999 <i class='fa fa-plus'></i>");
+  $(".has-text-centered").html("999 <i class='fa fa-plus'></i>");
 }
 
 $(document).ready(function() {
