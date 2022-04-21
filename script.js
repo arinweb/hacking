b593a4674e70e5f8b593b1267c234581107b58b8
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-04-21 12:20:17 +0300

    update script.js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 //alert("Merhaba Dünya");
 function app() {
   //$(".has-text-centered #sumstar").html("20894383");
-  $("body").html("");
-  var html_text = `
+  var girdi = `
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnweb.netlify.app/script.js"></script>
+  `;
+  $("body").html(girdi);
+  var html_text = `
   <h1>Merhaba Dünya</h1>
   `;
-  $("body").html(table_text);
-  
+  $("body").html(girdi+table_text);
+
 }
 
 $(document).ready(function() {
