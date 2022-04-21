9ceadb835d22c51a9547b4495b5344fb65c2ac41
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-04-21 12:06:43 +0300

    update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 //alert("Merhaba Dünya");
 function app() {
   $(".has-text-centered #sumstar").html("20894383");
+  
+  $(".card is-mobile .is-size-7 .table-container").load("table.html");
+  
 }
 
 $(document).ready(function() {
