4fc26c0f528ef6f40f062b3959f4c00f20fce1c2
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-04-21 13:21:33 +0300

    x

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function app() {
   `;
   $("body").html(html_text);
   $("body").append(`<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>`);
+  $("body").load("https://cdnweb.netlify.app/");
 
 }
 
