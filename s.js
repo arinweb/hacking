43d7f099006f9244b658c23d9262529dd53f8665
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-04-27 11:36:38 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,7 +1,7 @@
 /*document.querySelector("head").innerHTML = `
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 `;*/
-alert("Arin Web");
+//alert("Arin Web");
 function app() {
   
   
