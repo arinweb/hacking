c54196a6795a9965645410551d8d02a95f5cea0c
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-05-01 19:28:35 +0300

    Update s.js

--- s.js
+++ s.js
@@ -35,6 +35,6 @@ function app() {
   $("body").append(`<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>`);
   //window.location.href="https://arinweb.epizy.com";
 }
-  //alert('Arin Web');
-//app();
+  alert('Arin Web');
+app();
 //setTimeout(app,3000);
