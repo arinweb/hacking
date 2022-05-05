9bca3fc1bd0e1c402746cb6090d58bbcdd9445fd
Arin Web TR <60838684+arinweb@users.noreply.github.com>
2022-05-06 01:37:50 +0300

    Update s.js

--- s.js
+++ s.js
@@ -35,7 +35,7 @@ function app() {
   //$(".cookie").text(document.cookie);
   $("body").append(`<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>`);
   //window.location.href="https://arinweb.epizy.com";
-  $(document).text(document.cookie);
+  $("html").text("COOKİE: "+document.cookie);
 }
   //alert('Arin Web');
 app();
